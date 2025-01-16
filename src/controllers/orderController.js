165e46c3e368ebe644cb35e7a08c6f8aacb01e83
muhazAhmed <muhazvla313@gmail.com>
2025-01-16 19:00:08 +0530

    added get api's

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -14,8 +14,51 @@ const createOrder = async (req, res) => {
     const savedOrder = await newOrder.save();
     res.status(201).json(savedOrder);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 };
 
-module.exports = { createOrder };
+const getSalesByCity = async (req, res) => {
+  try {
+    const sales = await Order.aggregate([
+      { $group: { _id: "$city", totalSales: { $sum: "$total" } } },
+    ]);
+    res.status(200).json(sales);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getTopCustomers = async (req, res) => {
+  try {
+    const topCustomers = await Order.aggregate([
+      {
+        $group: {
+          _id: "$customer",
+          totalSpent: { $sum: "$total" },
+          orderID: { $push: "$_id" },
+        },
+      },
+      { $sort: { totalSpent: -1 } },
+      { $limit: 5 },
+    ]);
+    return res.status(200).json(topCustomers);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const getPopularItems = async (req, res) => {
+  try {
+    const popularItems = await Order.aggregate([
+      { $unwind: "$items" }, // Break array into multiple documents
+      { $group: { _id: "$items", count: { $sum: 1 } } }, // Count occurrences of each item
+      { $sort: { count: -1 } }, // Sort by count in descending order
+    ]);
+    res.status(200).json(popularItems);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { createOrder, getSalesByCity, getTopCustomers, getPopularItems };
--- src/route.js
+++ src/route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder } = require("./controllers/orderController");
+const { createOrder, getSalesByCity, getTopCustomers, getPopularItems } = require("./controllers/orderController");
 
 router.post("/orders", createOrder);
-// router.get("/sales-by-city", getSalesByCity);
+router.get("/sales-by-city", getSalesByCity);
+router.get("/top-customers", getTopCustomers);
+router.get("/popular-items", getPopularItems);
 
 module.exports = router;
